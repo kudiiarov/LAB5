964eb322c08a6abf2573f012620d20452b9b9211
kudiiarov <90313702+kudiiarov@users.noreply.github.com>
2024-05-01 10:56:30 +0600

    Tests

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,89 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+class MainTest {
+
+    @Test
+    public void test1() {
+        String s = "{\"id\":11}";
+
+        given().
+                contentType("application/json").
+                body(s).
+                when().
+                post("https://petstore.swagger.io/v2/pet").
+                then().
+                statusCode(200).
+                body("id", equalTo(11));
+
+    }
+
+    @Test
+    public void testGet() {
+        RestAssured
+                .get("https://petstore.swagger.io/v2/pet/11")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .body("id", equalTo(11));
+    }
+
+    @Test
+    public void testGetNotFoundPet() {
+        RestAssured
+                .get("https://petstore.swagger.io/v2/pet/11")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testPut() {
+        String s = "{\"id\":11}";
+        given().
+                contentType("application/json").
+                body(s).
+                when().
+                put("https://petstore.swagger.io/v2/pet").
+                then().
+                statusCode(200).
+                body("id", equalTo(11));
+    }
+
+    @Test
+    public void testPutRequest() {
+
+        String requestBody = "{\"id\": 11, \"name\": \"snake\"}";
+
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .when()
+                .put("https://petstore.swagger.io/v2/pet")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("snake"))
+                .extract()
+                .response();
+    }
+
+    @Test
+    public void testDeleteRequestError() {
+
+        Response response;
+        response = given()
+                .when()
+                .delete("https://petstore.swagger.io/v2/pet/8")
+                .then()
+                .statusCode(404)
+                .extract()
+                .response();
+    }
+}
